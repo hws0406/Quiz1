1f1f69d7ab0f5ab06c33e02f4ead55b5dcdad27a
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-04-14 09:25:59 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+ublic class Main {
+
+	public static void main(String[] args) {
+		
+		Laptop d = new Laptop();
+		d.setBrand();
+		d.setModel();
+		d.setColor();
+		d.setProcessor();
+		d.setPrice();
+		
+		System.out.println("--LAPTOP 1--");
+		System.out.println("Brand: " + d.getBrand());
+		System.out.println("Model: " + d.getModel());
+		System.out.println("Color: " + d.getColor());
+		System.out.println("Processor: " + d.getProcessor());
+		System.out.println("Price: RM" + d.getPrice());
+		d.Promotion();
+		
+		System.out.println();
+		
+		Laptop v = new Laptop();
+		v.setBrand();
+		v.setModel();
+		v.setColor();
+		v.setProcessor();
+		v.setPrice();	
+		
+		System.out.println("--ASUS--");
+		System.out.println("Brand: " + v.getBrand());
+		System.out.println("Model: " + v.getModel());
+		System.out.println("Color: " + v.getColor());
+		System.out.println("Processor: " + v.getProcessor());
+		System.out.println("Price: RM" + v.getPrice());
+		d.Promotion();
+	}
+}
